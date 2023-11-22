cc7d3a167de48264be3322e805d3bc5956f10971
Ron Veasna <vaing133@gmail.com>
2023-11-22 19:30:42 +0700

    New Update

---
+++ Anime Sna/home.js
@@ -0,0 +1,57 @@
+function s(){
+    alert("Sorry we have no anime to store for search!")
+}
+function favor(){
+    alert("Sorry you can not save anime to your favorite!")
+}
+function login(){
+    alert("Sorry we don't have data to store your account sign up!")
+}
+function changeimage1(){
+    let displayImage = document.getElementById('img1')
+    if(displayImage.src.match('4.png')){
+        displayImage.src = '4.png'
+    } else {
+        displayImage.src = '4.png'
+    }
+}
+function changeimage2(){
+    let displayImage = document.getElementById('img1')
+    if(displayImage.src.match('4.png')){
+        displayImage.src = '5.png'
+    } else {
+        displayImage.src = '5.png'
+    }
+}
+function changeimage3(){
+    let displayImage = document.getElementById('img1')
+    if(displayImage.src.match('4.png')){
+        displayImage.src = '6.png'
+    } else {
+        displayImage.src = '6.png'
+    }
+}
+function changeimage4(){
+    let displayImage = document.getElementById('img1')
+    if(displayImage.src.match('4.png')){
+        displayImage.src = '7.png'
+    } else {
+        displayImage.src = '7.png'
+    }
+}
+function changeimage5(){
+    let displayImage = document.getElementById('img1')
+    if(displayImage.src.match('4.png')){
+        displayImage.src = '8.png'
+    } else {
+        displayImage.src = '8.png'
+    }
+}
+function changeimage6(){
+    let displayImage = document.getElementById('img1')
+    if(displayImage.src.match('4.png')){
+        displayImage.src = '9.png'
+    } else {
+        displayImage.src = '9.png'
+    }
+}
